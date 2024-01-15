d756651f5e762bf70b39f06ffba715796da8b563
Petr Machačka <91247837+PetrMachacka@users.noreply.github.com>
2024-01-15 19:44:33 +0100

    initial

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QRScanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace QRScanner;
+using Microsoft.Maui.Controls;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnPhotoClicked(object sender, EventArgs e)
+    {
+        TakePhoto();
+    }
+
+    private void OnScanClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    public async void TakePhoto()
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+            if (photo != null)
+            {
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                await sourceStream.CopyToAsync(localFileStream);
+
+                capturedImage.Source = localFilePath;
+            }
+        }
+    }
+
+}
